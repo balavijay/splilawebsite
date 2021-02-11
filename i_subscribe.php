6094255cecce3b87c149cf2df1a1883a3237cf1f
Balavijay <balavijay@gmail.com>
2021-02-11 10:59:49 +0530

    Captcha implementation

--- i_subscribe.php
+++ i_subscribe.php
@@ -1,10 +1,34 @@
-<script src="https://www.google.com/recaptcha/api.js"></script>
-<script>
-   function onSubmit(token) {
-        
-     // document.getElementById("subscribeForm").submit();
-   }
- </script>
+<script type="text/javascript">
+      var onloadCallback = function() {
+        console.log("text", this);
+        console.log(grecaptcha);
+        grecaptcha.render('html_element', {
+          'sitekey' : '6LfFTEEaAAAAAO9jAFO_xFeZH6L4Z8c1JOdaP_pY'
+        });
+      };
+
+      function get_action(form) 
+      {
+          var v = grecaptcha.getResponse();
+          if(v.length == 0)
+          {
+              document.getElementById('captcha').innerHTML="Please validate Captcha";
+              return false;
+          }
+          else
+          {
+              document.getElementById('captcha').innerHTML="Captcha completed";
+              document.getElementById("subscribeForm").submit();
+              return true; 
+          }
+      }
+    </script>
+
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+</script>
+
+
                     <div class="list-group custom-list-group ql-section" >									
 
                         <div class="card-body">
@@ -22,10 +46,14 @@
                                     <input type="email" class="form-control" name="email" value="" placeholder="Enter Your Email" required="">
                                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                     <br />
-                                    <button class="g-recaptcha" 
-                                        data-sitekey="6Lepqg4aAAAAANkOIZHi_GuchxmFjpdTvCK0-uTI" 
-                                        data-callback='onSubmit' 
-                                        data-action='submit'>Submit</button>
+
+                                    <div id="html_element"></div>
+                                    <div id="captcha"></div>
+
+                                    <!--div class="g-recaptcha" data-sitekey="6LfFTEEaAAAAAO9jAFO_xFeZH6L4Z8c1JOdaP_pY"></div-->
+
+                                    <button  
+                                        type='button' onclick="javascript:get_action();">Submit</button>
 
                                 </div>
 
